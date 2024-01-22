48872f36f21b026d069d8779d737686fb15f8890
mateuszparasiewicz <m.parasiewicz00@gmail.com>
2024-01-22 20:28:29 +0100

    Update - Mockito and Jacoco learn

--- src/main/java/pl/kurs/LibraryService/MyException/UserFoundException.java
+++ src/main/java/pl/kurs/LibraryService/MyException/UserFoundException.java
@@ -1,7 +1,6 @@
 package pl.kurs.LibraryService.MyException;
 
 public class UserFoundException extends RuntimeException {
-
     public UserFoundException(String message) {
         super(message);
     }
